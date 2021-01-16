d587ac3da03e52e7dffc951e46b881d15e5f6cb3
DEV KUMAR <69479193+devkumar326@users.noreply.github.com>
2021-01-16 22:03:49 +0530

    Update removeduplicate.cpp

--- Leetcode_Easy/Arrays/removeduplicate.cpp
+++ Leetcode_Easy/Arrays/removeduplicate.cpp
@@ -1,3 +1,15 @@
+// Remove Duplicates from Sorted Array
+
+//Question
+//Given a sorted array nums, remove the duplicates in-place such that each element appears only once and returns the new length.
+//Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+//Example
+//Input: nums = [1,1,2]
+//Output: 2, nums = [1,2]
+//Explanation: Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively. It doesn't matter what you leave beyond the returned length.
+
+//Code:
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
@@ -5,7 +17,7 @@ public:
             return 0;
         }
         int i=0;
-        for (int j=1; j<nums.size();j++) {
+        for (int j=1; j<nums.size(); j++) {
             if(nums[i]!=nums[j]){
                 i++;
                 nums[i]=nums[j];
@@ -14,3 +26,6 @@ public:
         return i+1;
     }
 };
+
+//Time Complexity: O(N)
+//Space Complexity: O(1)
