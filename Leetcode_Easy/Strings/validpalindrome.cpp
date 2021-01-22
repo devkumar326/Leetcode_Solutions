5ec3346e99836dfc08b5a3a283e005b21600ff87
DEV KUMAR <69479193+devkumar326@users.noreply.github.com>
2021-01-22 17:01:43 +0530

    Create validpalindrome.cpp

---
+++ Leetcode_Easy/Strings/validpalindrome.cpp
@@ -0,0 +1,55 @@
+// Valid Palindrome
+
+//Question
+//Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+//Note: For the purpose of this problem, we define empty string as valid palindrome.
+
+//Example
+//Input: "A man, a plan, a canal: Panama"
+//Output: true
+
+//Example
+//Input: "race a car"
+//Output: false
+
+//Constraints:
+//s consists only of printable ASCII characters.
+
+//Code
+class Solution {
+public:
+    char isAlpha(char c)
+    {
+        if( (c>='0' && c<='9') || (c>='a' && c<='z') || (c>='A' && c<='Z'))
+            return true;
+        return false;
+    }
+    char toLower(char c)
+    {
+        if(c>='A' && c<='Z')
+            return (c+('a'-'A'));
+        return c;
+    }
+    bool isPalindrome(string s) {
+        if(s.size() == 0)
+            return true;
+        string ss;
+        for(int i = 0; i < s.size(); i++)
+        {
+            if(isAlpha(s[i]))
+            {
+                ss.append(1, toLower(s[i]));
+            }
+        }
+        int bg = 0; int ed = ss.size() - 1;
+        while(bg<ed)
+        {
+            if(ss[bg] != ss[ed])
+                return false;
+            bg++; ed--;
+        }
+        return true;
+    }
+};
+
+//Time Complexity: O(N)
